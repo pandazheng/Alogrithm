ec2f809778489ad07176f081a4000d16c620521a
pandazheng <543828315@qq.com>
2015-05-10 22:09:39 +0800

    静态顺序表实例
    静态顺序表实例

--- SqListDemo/SqListDemo/main.c
+++ SqListDemo/SqListDemo/main.c
@@ -8,8 +8,76 @@
 
 #include <stdio.h>
 
+#define MAXSIZE 10
+
+/*静态顺序表的各种操作*/
+
+//插入元素
+void insertElem(int Sqlist[],int *len,int i,int x)
+{
+    int t;
+    
+    if ( i < 1 || i > *len)
+    {
+        printf("插入元素位置错误\n");
+    }
+    
+    for (t = *len - 1 ; t >= i - 1 ; t--)
+        Sqlist[t+1] = Sqlist[t];
+    
+    Sqlist[i-1] = x;
+    
+    *len = *len + 1;
+}
+
+//删除元素 删除第i个元素
+void DeleteElem(int Sqlist[],int *len,int i)
+{
+    int t;
+    
+    if (i < 1 || i > *len - 1)
+    {
+        printf("删除元素的位置错误");
+    }
+    
+    for (t = i ; t <= *len - 1 ; t++)
+        Sqlist[t - 1] = Sqlist[t];
+    
+    *len = *len - 1;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int Sqlist[MAXSIZE];
+    int i;
+    int len;
+    
+    printf("输入6个整数: ");
+    for (i = 0 ; i < 6 ; i++)
+    {
+        scanf("%d",&Sqlist[i]);
+    }
+    
+    len = 6;
+    printf("SqList:\n");
+    for (i = 0 ; i < 6 ; i++)
+    {
+        printf("%d ",Sqlist[i]);
+    }
+    printf("\n");
+    
+    insertElem(Sqlist, &len, 3, 5);
+    for (i = 0 ; i < len ; i++)
+    {
+        printf("%d ",Sqlist[i]);
+    }
+    printf("\n");
+    
+    DeleteElem(Sqlist, &len, 4);
+    for (i = 0 ; i < len ; i++)
+    {
+        printf("%d ",Sqlist[i]);
+    }
+    
     return 0;
 }
